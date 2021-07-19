93cc88eaa7c92cd91dfb3f4053fe7c7be6135e9c
JanKrb <jankrb@jankrb.com>
2021-07-19 20:13:00 +0200

    Implement LaraBug

--- config/logging.php
+++ config/logging.php
@@ -37,7 +37,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['single', 'larabug'],
             'ignore_exceptions' => false,
         ],
 
@@ -100,6 +100,8 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
-    ],
-
+        'larabug' => [
+            'driver' => 'larabug',
+        ]
+    ]
 ];
