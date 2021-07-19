96524196c386ba691916995453ae4f23d7d5da30
JanKrb <jankrb@jankrb.com>
2021-07-19 20:43:15 +0200

    Init inertiajs

---
+++ resources/js/components/pages/HelloWorld.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <p>Hello InertiaJS</p>
+    </div>
+</template>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Styles -->
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet" />
+    </head>
+    <body>
+        @inertia
+        @routes
+        @larabugJavaScriptClient
+
+        <!-- Scripts -->
+        <script src="{{ mix('js/app.js') }}" defer></script>
+    </body>
+</html>
