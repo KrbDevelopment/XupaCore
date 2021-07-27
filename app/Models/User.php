5952eaf88e999d3c1dfa7ed41fd9ed8c2ff9dd27
JanKrb <jankrb@jankrb.com>
2021-07-27 20:17:07 +0200

    BACK - Add First- & Last-name to users

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,9 @@ class User extends Authenticatable
     protected $fillable = [
         // Base
         'username',
+        'first_name',
+        'last_name',
+
         'email',
         'password',
 
