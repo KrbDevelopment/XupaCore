e1f00791da4671e8d597a977edc7a851e9218cef
JanKrb <jankrb@jankrb.com>
2021-07-29 15:07:02 +0200

    BACK - Verify Email

--- app/Http/Controllers/Authentication/EmailController.php
+++ app/Http/Controllers/Authentication/EmailController.php
@@ -3,18 +3,46 @@
 namespace App\Http\Controllers\Authentication;
 
 use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\VerifiesEmails;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class EmailController extends Controller
 {
+    use VerifiesEmails;
+
     /**
      * Render Auth-ForgotPassword page
      * @return Response Inertia Render Response
      */
-    public function renderEmailVerify(): Response
+    public function renderEmailVerify(Request $request): Response
     {
-        return Inertia::render('Authentication/VerifyEmail');
+        return Inertia::render('Authentication/VerifyEmail', [
+            'expires' => $request->expires,
+            'hash' => $request->hash,
+            'id' => $request->id,
+            'signature' => $request->signature
+        ]);
+    }
+
+    /**
+     * Where to redirect users after verification.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('signed')->only('verify');
+        $this->middleware('throttle:6,1')->only('verify', 'resend');
     }
 }
--- app/Http/Controllers/Authentication/RegisterController.php
+++ app/Http/Controllers/Authentication/RegisterController.php
@@ -63,12 +63,16 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'username' => $data['username'],
             'first_name' => $data['first_name'],
             'last_name' => $data['last_name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+        $user->sendEmailVerificationNotification();
+
+        return $user;
     }
 }
