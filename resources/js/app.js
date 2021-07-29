cf963266465e3514c3acec1bdbf858d48f8677bc
Nino Gralla <nino.gralla@gmx.de>
2021-07-29 15:54:32 +0200

    FRNT - Implement Toast Notifications

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,8 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
 import { InertiaProgress } from '@inertiajs/progress'
+import Notifications from 'vue3-vt-notifications'
+import NotificationComponent from './components/global/Notifications'
 
 // Init InertiaJs progressbar
 InertiaProgress.init()
@@ -10,6 +12,8 @@ createInertiaApp({
     setup({ el, app, props, plugin }) {
         createApp({ render: () => h(app, props) })
             .use(plugin)
+            .use(Notifications)
+            .component('Notification', NotificationComponent)
             .mount(el)
     }
 })
