42003cca455fa3e819a9708035742dbcf0278f9b
JanKrb <jankrb@jankrb.com>
2021-07-31 20:31:39 +0200

    BACK - Setup Spatie permissions

---
+++ database/seeders/CreatePermissionsSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class CreatePermissionsSeeder extends Seeder
+{
+    public $permissions = [
+        ''
+    ];
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
