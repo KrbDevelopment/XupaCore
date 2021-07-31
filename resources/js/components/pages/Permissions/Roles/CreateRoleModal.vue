50b26fb15d8d1ab35a56411489291fb23859334f
JanKrb <jankrb@jankrb.com>
2021-07-31 22:38:37 +0200

    FRNT - Create role modal

---
+++ resources/js/components/pages/Permissions/Roles/CreateRoleModal.vue
@@ -0,0 +1,224 @@
+<template>
+    <TransitionRoot as="template" :show="open">
+        <Dialog as="div" static class="fixed inset-0 overflow-hidden" @close="open = false" :open="open">
+            <div class="absolute inset-0 overflow-hidden">
+                <DialogOverlay class="absolute inset-0" />
+
+                <div class="fixed inset-y-0 pl-16 max-w-full right-0 flex">
+                    <TransitionChild as="template" enter="transform transition ease-in-out duration-500 sm:duration-700" enter-from="translate-x-full" enter-to="translate-x-0" leave="transform transition ease-in-out duration-500 sm:duration-700" leave-from="translate-x-0" leave-to="translate-x-full">
+                        <div class="w-screen max-w-md">
+                            <form class="h-full divide-y divide-gray-200 flex flex-col bg-white shadow-xl" @submit.prevent="createRole">
+                                <div class="flex-1 h-0 overflow-y-auto">
+                                    <div class="py-6 px-4 bg-xupa sm:px-6">
+                                        <div class="flex items-center justify-between">
+                                            <DialogTitle class="text-lg font-semibold text-white">
+                                                New Project
+                                            </DialogTitle>
+                                            <div class="ml-3 h-7 flex items-center">
+                                                <button type="button" class="bg-xupa rounded-md text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-white" @click="open = false">
+                                                    <span class="sr-only">Close panel</span>
+                                                    <XIcon class="h-6 w-6" aria-hidden="true" />
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <div class="mt-1">
+                                            <p class="text-sm text-white">
+                                                Get started by filling in the information below to create your new project.
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div class="flex-1 flex flex-col justify-between">
+                                        <div class="px-4 divide-y divide-gray-200 sm:px-6">
+                                            <div class="space-y-6 pt-6 pb-5">
+                                                <div>
+                                                    <label for="project-name" class="block text-sm font-medium text-gray-900">
+                                                        Role name <span class="text-xupa">*</span>
+                                                    </label>
+                                                    <div class="mt-1">
+                                                        <input type="text" name="role-name" id="role-name" :class="`block w-full shadow-sm sm:text-sm focus:ring-xupa focus:border-xupa border-gray-300 rounded-md ${this.validationErrors.name ? 'border-red-700' : ''}`" v-model="role.name" />
+                                                    </div>
+                                                    <p class="text-sm text-red-700 mt-1 float-right" v-if="this.validationErrors.name">{{ this.validationErrors.name }}</p>
+                                                </div>
+                                                <div>
+                                                    <label for="description" class="block text-sm font-medium text-gray-900">
+                                                        Description
+                                                    </label>
+                                                    <div class="mt-1">
+                                                        <textarea id="description" name="description" rows="4" class="block w-full shadow-sm sm:text-sm focus:ring-xupa focus:border-xupa border border-gray-300 rounded-md" />
+                                                    </div>
+                                                    <p class="text-sm text-red-700 mt-1 float-right" v-if="this.validationErrors.description">{{ this.validationErrors.description }}</p>
+                                                </div>
+                                                <fieldset>
+                                                    <legend class="text-sm font-medium text-gray-900">
+                                                        Guard <span class="text-xupa">*</span>
+                                                    </legend>
+                                                    <div class="mt-2 space-y-5">
+                                                        <div class="relative flex items-start" v-for="guard in guards" v-bind:key="guard">
+                                                            <div class="absolute flex items-center h-5">
+                                                                <input :id="guard" :name="guard" :value="guard" aria-describedby="privacy-public-description" type="radio" class="focus:ring-xupa h-4 w-4 text-xupa border-gray-300" v-model="role.guard_name" />
+                                                            </div>
+                                                            <div class="pl-7 text-sm">
+                                                                <label :for="guard" class="font-medium text-gray-900 capitalize">
+                                                                    {{ guard }}-Guard
+                                                                </label>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </fieldset>
+                                                <p class="text-sm text-red-700 mt-1 float-right" v-if="this.validationErrors.guard_name">{{ this.validationErrors.guard_name }}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="flex-shrink-0 px-4 py-4 flex justify-end">
+                                    <button type="button" class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-xupa hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-xupa" @click="open = false">
+                                        Cancel
+                                    </button>
+                                    <button type="submit" class="ml-4 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-xupa hover:bg-xupa-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-xupa">
+                                        Create
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </TransitionChild>
+                </div>
+            </div>
+        </Dialog>
+    </TransitionRoot>
+</template>
+
+<script>
+import { ref } from 'vue'
+import { Dialog, DialogOverlay, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
+import { XIcon } from '@heroicons/vue/outline'
+import { Inertia } from '@inertiajs/inertia'
+
+const team = [
+    {
+        name: 'Tom Cook',
+        email: 'tomcook@example.com',
+        href: '#',
+        imageUrl:
+            'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+    },
+    {
+        name: 'Whitney Francis',
+        email: 'whitneyfrancis@example.com',
+        href: '#',
+        imageUrl:
+            'https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+    },
+    {
+        name: 'Leonard Krasner',
+        email: 'leonardkrasner@example.com',
+        href: '#',
+        imageUrl:
+            'https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+    },
+    {
+        name: 'Floyd Miles',
+        email: 'floydmiles@example.com',
+        href: '#',
+        imageUrl:
+            'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+    },
+    {
+        name: 'Emily Selman',
+        email: 'emilyselman@example.com',
+        href: '#',
+        imageUrl:
+            'https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+    }
+]
+
+export default {
+    props: ['guards'],
+    components: {
+        Dialog,
+        DialogOverlay,
+        DialogTitle,
+        TransitionChild,
+        TransitionRoot,
+        XIcon
+    },
+    data() {
+        return {
+            role: {
+                name: '',
+                description: '',
+                guard_name: ''
+            },
+            validationErrors: {}
+        }
+    },
+    setup() {
+        const open = ref(false)
+
+        return {
+            team,
+            open
+        }
+    },
+    methods: {
+        route(name, options = {}) {
+            return window.route(name, options)
+        },
+        createRole() {
+            this.validationErrors = {}
+
+            Inertia.post(this.route('permissions.requests.role.create'), this.role, {
+                preserveScroll: true, // Keep scrolling position (freeze position)
+
+                /**
+                 * Successful server response [HTTP Code: 2x]
+                 */
+                onSuccess: () => {
+                    this.$notify(
+                        {
+                            group: 'success',
+                            text: 'Role has been created successfully',
+                            transitionGroupClasses: {
+                                enterActiveClassDelayed: 'transform ease-out duration-300 transition delay-300',
+                                enterActiveClass: 'transform ease-out duration-300 transition',
+                                enterFromClass: 'translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-4',
+                                enterToClass: 'translate-y-0 opacity-100 sm:translate-x-0',
+                                leaveActiveClass: 'transition ease-in duration-500',
+                                leaveFromClass: 'opacity-100',
+                                leaveToClass: 'opacity-0',
+                                moveClass: 'transition duration-500'
+                            }
+                        },
+                        4000
+                    )
+                    this.open = false
+                },
+
+                /**
+                 * Failed server response [HTTP Code: 4x & 5x] Most likely validation error
+                 */
+                onError: (err) => {
+                    this.validationErrors = err
+                    this.$notify(
+                        {
+                            group: 'error',
+                            title: 'An error has occurred',
+                            text: 'We weren\'t able to create the role',
+                            transitionGroupClasses: {
+                                enterActiveClassDelayed: 'transform ease-out duration-300 transition delay-300',
+                                enterActiveClass: 'transform ease-out duration-300 transition',
+                                enterFromClass: 'translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-4',
+                                enterToClass: 'translate-y-0 opacity-100 sm:translate-x-0',
+                                leaveActiveClass: 'transition ease-in duration-500',
+                                leaveFromClass: 'opacity-100',
+                                leaveToClass: 'opacity-0',
+                                moveClass: 'transition duration-500'
+                            }
+                        },
+                        4000
+                    )
+                }
+            })
+        }
+    }
+}
+</script>
