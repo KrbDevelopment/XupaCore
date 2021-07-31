2a129efadb375342a598595657c14e86f05786ad
JanKrb <jankrb@jankrb.com>
2021-07-31 12:20:20 +0200

    BACK - Add functions to dismiss/delete notifications

--- app/Http/Controllers/Core/NotificationController.php
+++ app/Http/Controllers/Core/NotificationController.php
@@ -16,6 +16,7 @@ class NotificationController extends Controller
      * Function to mark a single notification as read
      * @param Request $request
      * -> Notification: Required-Integer-Notification_Primary
+     * @return RedirectResponse
      */
     public function markAsReadSingle(Request $request): RedirectResponse
     {
@@ -32,10 +33,11 @@ class NotificationController extends Controller
     }
 
     /**
-     * Function to mark a array of notification id's as read
+     * Function to mark an array of notification id's as read
      * @param Request $request
      * -> Notifications: Required-Array
      * ---> Notification: Required-Integer-Notification_Primary
+     * @return RedirectResponse
      */
     public function markAsReadArray(Request $request): RedirectResponse
     {
@@ -49,6 +51,46 @@ class NotificationController extends Controller
             $notification = Notification::findOrFail($notification);
             $notification->read = $request->has('state') ? $request->state : true;
             $notification->save();
+        }
+
+        return back();
+    }
+
+    /**
+     * Function to delete a single notification by id
+     * @param Request $request
+     * -> Notification: Required-Integer-Notification_Primary
+     * @return RedirectResponse
+     */
+    public function deleteNotificationsSingle(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'notification' => 'required|int|exists:notifications,id'
+        ]);
+
+        $notification = Notification::findOrFail($request->notification);
+        $notification->delete();
+
+        return back();
+    }
+
+    /**
+     * Function to delete an array of notification id's
+     * @param Request $request
+     * -> Notifications: Required-Array
+     * ---> Notification: Required-Integer-Notification_Primary
+     * @return RedirectResponse
+     */
+    public function deleteNotificationsArray(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'notifications' => 'required|array',
+            'notifications.*' => 'required|int|exists:notifications,id'
+        ]);
+
+        foreach ($request->notifications as $notification) {
+            $notification = Notification::findOrFail($notification);
+            if ($notification->dismissible) $notification->delete();
         }
 
         return back();
