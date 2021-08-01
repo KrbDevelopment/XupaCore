00d7798e178523d506e1d96e3cb815875f4655f7
JanKrb <jankrb@jankrb.com>
2021-08-01 21:54:29 +0200

    FRNT - Optimize imports & restructure

--- resources/js/components/pages/Permissions/Roles/Detail.vue
+++ resources/js/components/pages/Permissions/Roles/Detail.vue
@@ -18,13 +18,37 @@
                         <p class="text-lg font-medium text-blue-gray-900">Administrator</p>
                     </div>
                     <div class="flex-1 min-h-0 overflow-y-auto">
-                        <a v-for="item in subNavigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-blue-50 bg-opacity-50' : 'hover:bg-blue-50 hover:bg-opacity-50', 'flex p-6 border-b border-blue-gray-200']" :aria-current="item.current ? 'page' : undefined">
-                            <component :is="item.icon" class="flex-shrink-0 -mt-0.5 h-6 w-6 text-blue-gray-400" aria-hidden="true" />
+                        <Link as="a" :href="this.route('permissions.render.roles.detail.settings', { role: this.role.id })" class="hover:bg-blue-50 hover:bg-opacity-50 flex p-6 border-b border-blue-gray-200">
+                            <CogIcon class="flex-shrink-0 -mt-0.5 h-6 w-6 text-blue-gray-400" aria-hidden="true" />
                             <div class="ml-3 text-sm">
-                                <p class="font-medium text-blue-gray-900">{{ item.name }}</p>
-                                <p class="mt-1 text-blue-gray-500">{{ item.description }}</p>
+                                <p class="font-medium text-blue-gray-900">Settings</p>
+                                <p class="mt-1 text-blue-gray-500">Here you can see and edit the basic role preferences</p>
                             </div>
-                        </a>
+                        </Link>
+
+                        <Link as="a" :href="this.route('permissions.render.roles.detail.access', { role: this.role.id })" class="hover:bg-blue-50 hover:bg-opacity-50 flex p-6 border-b border-blue-gray-200">
+                            <KeyIcon class="flex-shrink-0 -mt-0.5 h-6 w-6 text-blue-gray-400" aria-hidden="true" />
+                            <div class="ml-3 text-sm">
+                                <p class="font-medium text-blue-gray-900">Access</p>
+                                <p class="mt-1 text-blue-gray-500">Here you can see current members of this role and invite new users</p>
+                            </div>
+                        </Link>
+
+                        <Link as="a" :href="this.route('permissions.render.roles.detail.notifications', { role: this.role.id })" class="hover:bg-blue-50 hover:bg-opacity-50 flex p-6 border-b border-blue-gray-200">
+                            <BellIcon class="flex-shrink-0 -mt-0.5 h-6 w-6 text-blue-gray-400" aria-hidden="true" />
+                            <div class="ml-3 text-sm">
+                                <p class="font-medium text-blue-gray-900">Notifications</p>
+                                <p class="mt-1 text-blue-gray-500">Here you can see current members of this role and invite new users</p>
+                            </div>
+                        </Link>
+
+                        <Link as="a" :href="this.route('permissions.render.roles.detail.integrations', { role: this.role.id })" class="hover:bg-blue-50 hover:bg-opacity-50 flex p-6 border-b border-blue-gray-200">
+                            <ViewGridAddIcon class="flex-shrink-0 -mt-0.5 h-6 w-6 text-blue-gray-400" aria-hidden="true" />
+                            <div class="ml-3 text-sm">
+                                <p class="font-medium text-blue-gray-900">Integrations</p>
+                                <p class="mt-1 text-blue-gray-500">Here you can see the integrations, which are in use for your role</p>
+                            </div>
+                        </Link>
                     </div>
                 </nav>
 
@@ -39,69 +63,27 @@
 
 <script>
 import DashboardLayout from '../../Layout/Dashboard'
-import { Dialog, DialogOverlay, TransitionChild, TransitionRoot } from '@headlessui/vue'
-import {
-    BellIcon,
-    CogIcon,
-    KeyIcon,
-    MenuIcon,
-    ViewGridAddIcon,
-    XIcon
-} from '@heroicons/vue/outline'
 import { ChevronLeftIcon } from '@heroicons/vue/solid'
+import { CogIcon, KeyIcon, BellIcon, ViewGridAddIcon } from '@heroicons/vue/outline'
+import { Link } from '@inertiajs/inertia-vue3'
 
 export default {
     props: ['role'],
     components: {
         DashboardLayout,
-        Dialog,
-        DialogOverlay,
-        TransitionChild,
-        TransitionRoot,
         ChevronLeftIcon,
-        MenuIcon,
-        XIcon
+        Link,
+        CogIcon,
+        KeyIcon,
+        BellIcon,
+        ViewGridAddIcon
     },
     data() {
-        return {
-            subNavigation: [
-                {
-                    name: 'Settings',
-                    description: 'Here you can see and edit the basic role preferences',
-                    href: this.route('permissions.render.roles.detail.settings', { role: this.role.id }),
-                    icon: CogIcon,
-                    current: true
-                },
-                {
-                    name: 'Access',
-                    description: 'Here you can see current members of this role and invite new users',
-                    href: this.route('permissions.render.roles.detail.access', { role: this.role.id }),
-                    icon: KeyIcon,
-                    current: false
-                },
-                {
-                    name: 'Notifications',
-                    description: 'Here you can see and create role related notifications to the members',
-                    href: this.route('permissions.render.roles.detail.notifications', { role: this.role.id }),
-                    icon: BellIcon,
-                    current: false
-                },
-                {
-                    name: 'Integrations',
-                    description: 'Here you can see the integrations, which are in use for your role',
-                    href: this.route('permissions.render.roles.detail.integrations', { role: this.role.id }),
-                    icon: ViewGridAddIcon,
-                    current: false
-                }
-            ]
-        }
+        return {}
     },
     methods: {
         route(name, options = {}) {
             return window.route(name, options)
-        },
-        isCurrent(name) {
-            return window.route().current(name)
         }
     }
 }
