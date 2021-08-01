30014358d26e625d4d2c72f43103a39442006a90
Nino Gralla <nino.gralla@gmx.de>
2021-08-01 21:42:27 +0200

    FRNT fix Accounts Overview & add Delete function

--- resources/js/components/pages/Accounts/Overview.vue
+++ resources/js/components/pages/Accounts/Overview.vue
@@ -15,7 +15,7 @@
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16v2a2 2 0 01-2 2H5a2 2 0 01-2-2v-7a2 2 0 012-2h2m3-4H9a2 2 0 00-2 2v7a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-1m-1 4l-3 3m0 0l-3-3m3 3V3"></path>
                                 </svg>
                             </div>
-                            <div class="text-red-500 p-2 border-transparent border bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 hover:bg-gray-200 cursor-pointer rounded focus:outline-none focus:border-gray-800 focus:shadow-outline-gray" @click="massDeleteRoles">
+                            <div class="text-red-500 p-2 border-transparent border bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 hover:bg-gray-200 cursor-pointer rounded focus:outline-none focus:border-gray-800 focus:shadow-outline-gray" @click="massDeleteAccounts">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="icon cursor-pointer icon-tabler icon-tabler-trash" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                     <path stroke="none" d="M0 0h24v24H0z" />
                                     <line x1="4" y1="7" x2="20" y2="7" />
@@ -29,14 +29,14 @@
                     </div>
                     <div class="w-full lg:w-2/3 flex flex-col lg:flex-row items-start lg:items-center justify-end">
                         <div class="flex items-center py-3 lg:py-0 lg:px-6">
-                            <p class="text-base text-gray-600 dark:text-gray-400" id="page-view">Viewing {{ this.accounts.length }} - {{ this.accounts.length }} of {{ this.accounts.length }}</p>
-                            <Link as="a" :href="this.accounts" v-if="this.accounts" class="text-gray-600 dark:text-gray-400 ml-2 border-transparent cursor-pointer rounded">
+                            <p class="text-base text-gray-600 dark:text-gray-400" id="page-view">Viewing {{ this.accounts.from }} - {{ this.accounts.to }} of {{ this.accounts.total }}</p>
+                            <Link as="a" :href="this.accounts.prev_page_url" v-if="this.accounts.prev_page_url" class="text-gray-600 dark:text-gray-400 ml-2 border-transparent cursor-pointer rounded">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-left" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                     <path stroke="none" d="M0 0h24v24H0z" />
                                     <polyline points="15 6 9 12 15 18" />
                                 </svg>
                             </Link>
-                            <Link as="a" :href="this.accounts" v-if="this.accounts" class="text-gray-600 dark:text-gray-400 border-transparent rounded focus:outline-none cursor-pointer">
+                            <Link as="a" :href="this.accounts.next_page_url" v-if="this.accounts.next_page_url" class="text-gray-600 dark:text-gray-400 border-transparent rounded focus:outline-none cursor-pointer">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevron-right" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                     <path stroke="none" d="M0 0h24v24H0z" />
                                     <polyline points="9 6 15 12 9 18" />
@@ -63,12 +63,12 @@
                         </tr>
                         </thead>
                         <tbody>
-                        <tr class="h-24 border-gray-300 border-b" v-for="(account, index) in this.accounts" v-bind:key="account.id">
+                        <tr class="h-24 border-gray-300 border-b" v-for="(account, index) in this.accounts.data" v-bind:key="account.id">
                             <td class="pl-8 pr-6 text-left whitespace-no-wrap text-sm text-gray-800 dark:text-gray-100 tracking-normal leading-4">
                                 <input type="checkbox" class="cursor-pointer relative w-5 h-5 border rounded border-gray-400 bg-white dark:bg-gray-800 outline-none" v-model="selected" :value="index" />
                             </td>
                             <td class="text-sm pr-6 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4">
-                                <img :src="this.route('user.profile_image', account.id) ?? AlternativeProfilePicture" alt="" class="h-12 w-12 object-cover rounded-full">
+                                <img :src="this.route('user.profile_image', account) ?? AlternativeProfilePicture" alt="" class="h-12 w-12 object-cover rounded-full">
                             </td>
                             <td class="text-sm pr-6 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4">{{ account.first_name }}</td>
                             <td class="text-sm pr-6 whitespace-no-wrap text-gray-800 dark:text-gray-100 tracking-normal leading-4">{{ account.last_name }}</td>
@@ -123,6 +123,7 @@ import CreateAccountModal from './Components/CreateAccountModal'
 import moment from 'moment'
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
 import { Link } from '@inertiajs/inertia-vue3'
+import { Inertia } from '@inertiajs/inertia'
 
 export default {
     props: ['accounts'],
@@ -149,11 +150,14 @@ export default {
     watch: {
         allSelected: function (newVal) {
             if (newVal) {
-                this.accounts.forEach((notification, index) => this.selected.push(index))
+                this.accounts.data.forEach((notification, index) => this.selected.push(index))
             } else {
                 this.selected = []
             }
         }
+    },
+    mounted() {
+        console.log(this.accounts)
     },
     methods: {
         route(name, options = {}) {
@@ -167,7 +171,61 @@ export default {
         },
         getSelectedIds() {
             return this.selected.map((index) => {
-                return this.accounts[index].id
+                return this.accounts.data[index].id
+            })
+        },
+        massDeleteAccounts() {
+            const ids = this.getSelectedIds()
+
+            Inertia.delete(this.route('accounts.requests.account.delete.array', { accounts: ids }), {
+                preserveScroll: true, // Keep scrolling position (freeze position)
+
+                /**
+                 * Successful server response [HTTP Code: 2x]
+                 */
+                onSuccess: () => {
+                    this.$notify(
+                        {
+                            group: 'success',
+                            text: `${ids.length} accounts has been deleted`,
+                            transitionGroupClasses: {
+                                enterActiveClassDelayed: 'transform ease-out duration-300 transition delay-300',
+                                enterActiveClass: 'transform ease-out duration-300 transition',
+                                enterFromClass: 'translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-4',
+                                enterToClass: 'translate-y-0 opacity-100 sm:translate-x-0',
+                                leaveActiveClass: 'transition ease-in duration-500',
+                                leaveFromClass: 'opacity-100',
+                                leaveToClass: 'opacity-0',
+                                moveClass: 'transition duration-500'
+                            }
+                        },
+                        4000
+                    )
+                },
+
+                /**
+                 * Failed server response [HTTP Code: 4x & 5x] Most likely validation error
+                 */
+                onError: (err) => {
+                    console.log(err)
+                    this.$notify(
+                        {
+                            group: 'error',
+                            title: 'An error has occurred',
+                            transitionGroupClasses: {
+                                enterActiveClassDelayed: 'transform ease-out duration-300 transition delay-300',
+                                enterActiveClass: 'transform ease-out duration-300 transition',
+                                enterFromClass: 'translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-4',
+                                enterToClass: 'translate-y-0 opacity-100 sm:translate-x-0',
+                                leaveActiveClass: 'transition ease-in duration-500',
+                                leaveFromClass: 'opacity-100',
+                                leaveToClass: 'opacity-0',
+                                moveClass: 'transition duration-500'
+                            }
+                        },
+                        4000
+                    )
+                }
             })
         }
     }
