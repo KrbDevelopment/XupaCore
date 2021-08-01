b3389cb866a8794c55ca1b04bd94e2dca90fc725
JanKrb <jankrb@jankrb.com>
2021-08-01 13:10:59 +0200

    FRNT - Add empty detail pages

---
+++ resources/js/components/pages/Permissions/Permissions/Detail.vue
@@ -0,0 +1,15 @@
+<template>
+    <DashboardLayout>
+        <p>Permissions->Detail</p>
+    </DashboardLayout>
+</template>
+
+<script>
+import DashboardLayout from '../../Layout/Dashboard'
+
+export default {
+    components: {
+        DashboardLayout
+    }
+}
+</script>
