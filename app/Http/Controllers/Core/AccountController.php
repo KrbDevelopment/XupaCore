5379e6bec6534b6bb032ef8c96fe1cb903e50d28
Nino Gralla <nino.gralla@gmx.de>
2021-08-01 22:10:01 +0200

    BACK - add Create Account function

--- app/Http/Controllers/Core/AccountController.php
+++ app/Http/Controllers/Core/AccountController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers\Core;
 
 use App\Http\Controllers\Controller;
-use App\Models\Notification;
 use App\Models\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -21,7 +22,66 @@ class AccountController extends Controller
     public function renderAccountsOverview(): Response
     {
         return Inertia::render('Accounts/Overview',  [
-            'accounts' => User::all()
+            'accounts' => User::paginate(15)
         ]);
+    }
+
+    /**
+     * Delete array of user accounts
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function delete_account_single(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'account' => 'required|int|exists:users,id'
+        ]);
+
+        $account = User::findOrFail($request->account);
+        $account->delete();
+
+        return back();
+    }
+
+    /**
+     * Delete array of user accounts
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function delete_account_array(Request $request): RedirectResponse
+    {
+        $request->validate([
+           'accounts' => 'required|array',
+           'accounts.*' => 'required|int|exists:users,id'
+        ]);
+
+        foreach ($request->accounts as $account) {
+            $account = User::findOrFail($account);
+            $account->delete();
+        }
+
+        return back();
+    }
+
+    public function createAccount(Request $request): RedirectResponse
+    {
+        $request->validate([
+           'first_name' => 'required|string|max:255',
+           'last_name' => 'required|string|max:255',
+           'username' => 'required|string|max:255|unique:users,username',
+           'email' => 'required|string|max:255|unique:users,email',
+           'password' => 'required|string|max:255|confirmed'
+        ]);
+
+        $user = new User;
+        $user->first_name = $request->first_name;
+        $user->last_name = $request->last_name;
+        $user->username = $request->username;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+
+        $user->save();
+
+        return back();
     }
 }
