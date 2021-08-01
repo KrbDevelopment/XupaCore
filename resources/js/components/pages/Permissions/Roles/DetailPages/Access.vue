d22f8fe2e06b9734653ad0c10105cc4f3ad8e621
JanKrb <jankrb@jankrb.com>
2021-08-01 21:55:14 +0200

    FRNT - Add empty access page

---
+++ resources/js/components/pages/Permissions/Roles/DetailPages/Access.vue
@@ -0,0 +1,16 @@
+<template>
+    <Detail :role="this.role">
+        <form class="space-y-8 divide-y divide-gray-200">
+            
+        </form>
+    </Detail>
+</template>
+
+<script>
+import Detail from '../Detail'
+export default {
+    props: ['role'],
+    name: 'Settings',
+    components: { Detail }
+}
+</script>
