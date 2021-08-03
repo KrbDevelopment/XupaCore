a4d9aa1588924a4287088f59d56f305b80ea5cd6
Nino Gralla <nino.gralla@gmx.de>
2021-08-03 13:57:36 +0200

    BACK - add Password changed Notification function to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Notifications\PasswordChangedNotification;
 use App\Notifications\ResetPasswordNotification;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -69,6 +70,11 @@ class User extends Authenticatable
     public function sendPasswordResetNotification($token)
     {
         $this->notify(new ResetPasswordNotification($token, $this->email));
+    }
+
+    public function sendChangedPasswordNotification()
+    {
+        $this->notify(new PasswordChangedNotification());
     }
 
     /**
