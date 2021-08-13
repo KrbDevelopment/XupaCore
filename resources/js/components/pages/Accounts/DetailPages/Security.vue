95bf2b987920fef6ce566eea8844146cb0020646
Nino Gralla <nino.gralla@gmx.de>
2021-08-13 20:11:18 +0200

    FRNT - remove Unused button/function (Reset Password)

--- resources/js/components/pages/Accounts/DetailPages/Security.vue
+++ resources/js/components/pages/Accounts/DetailPages/Security.vue
@@ -67,17 +67,7 @@
                         <div class="container mx-auto flex items-start justify-center">
                             <div class="w-full">
                                 <div class="flex flex-col lg:flex-row mx-auto bg-white shadow rounded-lg">
-                                    <div class="w-full lg:w-1/3 px-12 flex flex-col items-center py-10">
-                                        <h2 class="text-gray-800 dark:text-gray-100 text-xl tracking-normal font-medium mb-1">Reset Password</h2>
-                                        <p class="flex text-gray-600 dark:text-gray-100 text-sm tracking-normal font-normal mb-3 text-center">
-                                            Email
-                                        </p>
-                                        <p class="text-gray-600 dark:text-gray-100 text-sm tracking-normal font-normal mb-8 text-center w-10/12">Send the user an email in which he can reset his password.</p>
-                                        <button type="submit" class="ml-3 inline-flex w-full justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-xupa hover:bg-xupa-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-xupa" @click="sendResetPasswordNotification(account)">
-                                            Send Notification
-                                        </button>
-                                    </div>
-                                    <div class="w-full lg:w-1/3 px-12 flex flex-col items-center py-10 border-t border-b lg:border-t-0 lg:border-b-0 lg:border-l border-gray-300">
+                                    <div class="w-full lg:w-1/2 px-12 flex flex-col items-center py-10 border-t border-b lg:border-t-0 lg:border-b-0 lg:border-l border-gray-300">
                                         <h2 class="text-gray-800 dark:text-gray-100 text-xl tracking-normal font-medium mb-1">Verify Mail</h2>
                                         <p class="flex text-gray-600 dark:text-gray-100 text-sm tracking-normal font-normal mb-3 text-center">
                                             Email
@@ -87,7 +77,7 @@
                                             Send Notification
                                         </button>
                                     </div>
-                                    <div class="w-full lg:w-1/3 px-12 flex flex-col items-center py-10 border-t border-b lg:border-t-0 lg:border-b-0 lg:border-l border-gray-300">
+                                    <div class="w-full lg:w-1/2 px-12 flex flex-col items-center py-10 border-t border-b lg:border-t-0 lg:border-b-0 lg:border-l border-gray-300">
                                         <h2 class="text-gray-800 dark:text-gray-100 text-xl tracking-normal font-medium mb-1">Password Changed</h2>
                                         <p class="flex text-gray-600 dark:text-gray-100 text-sm tracking-normal font-normal mb-3 text-center">
                                             Email
@@ -155,74 +145,6 @@ export default {
                             group: 'success',
                             title: 'Password changed',
                             text: 'the account password has been changed',
-                            transitionGroupClasses: {
-                                enterActiveClassDelayed: 'transform ease-out duration-300 transition delay-300',
-                                enterActiveClass: 'transform ease-out duration-300 transition',
-                                enterFromClass: 'translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-4',
-                                enterToClass: 'translate-y-0 opacity-100 sm:translate-x-0',
-                                leaveActiveClass: 'transition ease-in duration-500',
-                                leaveFromClass: 'opacity-100',
-                                leaveToClass: 'opacity-0',
-                                moveClass: 'transition duration-500'
-                            }
-                        },
-                        4000
-                    )
-                },
-
-                /**
-                 * Failed server response [HTTP Code: 4x & 5x] Most likely validation error
-                 * @param error
-                 */
-                onError: (error) => {
-                    this.validationError = error
-                    console.log(error)
-                    this.$notify(
-                        {
-                            group: 'error',
-                            title: 'An error has occurred',
-                            transitionGroupClasses: {
-                                enterActiveClassDelayed: 'transform ease-out duration-300 transition delay-300',
-                                enterActiveClass: 'transform ease-out duration-300 transition',
-                                enterFromClass: 'translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-4',
-                                enterToClass: 'translate-y-0 opacity-100 sm:translate-x-0',
-                                leaveActiveClass: 'transition ease-in duration-500',
-                                leaveFromClass: 'opacity-100',
-                                leaveToClass: 'opacity-0',
-                                moveClass: 'transition duration-500'
-                            }
-                        },
-                        4000
-                    )
-                }
-            })
-        },
-        /**
-         * Send a notification to the User (Reset Password)
-         */
-        sendResetPasswordNotification(user) {
-            // Clear recent validation errors
-            this.validationError = null
-
-            Inertia.post(window.route('accounts.requests.account.notify.reset_password', { user: user }), {
-
-                // CSRF Token
-                _token: this.$page.props.csrf_token
-            }, {
-                // Server Response Handler
-
-                preserveScroll: true, // Keep scrolling position (freeze position)
-
-                /**
-                 * Successfully server response
-                 * @param response
-                 */
-                onSuccess: (response) => {
-                    this.$notify(
-                        {
-                            group: 'success',
-                            title: 'Notification has been sent',
-                            text: 'The user will receive the notification shortly',
                             transitionGroupClasses: {
                                 enterActiveClassDelayed: 'transform ease-out duration-300 transition delay-300',
                                 enterActiveClass: 'transform ease-out duration-300 transition',
