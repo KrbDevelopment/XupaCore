b98960d72050f821861c78ca10d2eec8e48cec72
JanKrb <jankrb@jankrb.com>
2021-08-15 15:25:05 +0200

    Add timetracker to navigation

--- resources/js/components/pages/Layout/Dashboard.vue
+++ resources/js/components/pages/Layout/Dashboard.vue
@@ -108,10 +108,10 @@
                                     <XIcon class="block h-6 w-6" aria-hidden="true" />
                                 </button>
                             </div>
-                            <div class="mt-2 max-w-8xl mx-auto px-4 sm:px-6">
+                            <div class="my-4 max-w-8xl mx-auto px-4 sm:px-6">
                                 <div class="relative text-gray-400 focus-within:text-gray-500">
-                                    <label for="search" class="sr-only">Search all inboxes</label>
-                                    <input id="search" type="search" placeholder="Search all inboxes" class="block w-full border-gray-300 rounded-md pl-10 placeholder-gray-500 focus:border-xupa focus:ring-xupa" />
+                                    <label for="search" class="sr-only">Search</label>
+                                    <input id="search" type="search" placeholder="Search" class="block w-full border-gray-300 rounded-md pl-10 placeholder-gray-500 focus:border-xupa focus:ring-xupa" />
                                     <div class="absolute inset-y-0 left-0 flex items-center justify-center pl-3">
                                         <SearchIcon class="h-5 w-5" aria-hidden="true" />
                                     </div>
@@ -135,6 +135,11 @@
                                     <a v-for="item in userNavigation" :key="item.name" :href="item.href" class="block rounded-md py-2 px-3 text-base font-medium text-gray-900 hover:bg-gray-50">{{ item.name }}</a>
                                 </div>
                             </div>
+                            <div class="border-t border-gray-200 pt-4 pb-3">
+                                <div class="mt-3 max-w-8xl mx-auto px-2 space-y-1 sm:px-4">
+                                    <a v-for="item in sidebarNavigation" :key="item.name" :href="item.href" class="block rounded-md py-2 px-3 text-base font-medium text-gray-900 hover:bg-gray-50">{{ item.name }}</a>
+                                </div>
+                            </div>
                         </nav>
                     </TransitionChild>
                 </Dialog>
@@ -146,7 +151,7 @@
             <!-- Narrow sidebar-->
             <nav aria-label="Sidebar" class="md:block md:flex-shrink-0 md:bg-gray-800 md:overflow-y-auto hidden">
                 <div class="relative w-20 flex flex-col p-3 space-y-3">
-                    <a v-for="item in sidebarNavigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-400 hover:bg-gray-700', 'flex-shrink-0 inline-flex items-center justify-center h-14 w-14 rounded-lg']">
+                    <a v-for="item in sidebarNavigation" :key="item.name" :href="item.href" :class="[isCurrent(item.href) ? 'bg-gray-900 text-white' : 'text-gray-400 hover:bg-gray-700', 'flex-shrink-0 inline-flex items-center justify-center h-14 w-14 rounded-lg']">
                         <span class="sr-only">{{ item.name }}</span>
                         <component :is="item.icon" class="h-6 w-6" aria-hidden="true" />
                     </a>
@@ -177,14 +182,9 @@ import {
 } from '@headlessui/vue'
 import { ChevronDownIcon, SearchIcon } from '@heroicons/vue/solid'
 import {
-    ArchiveIcon,
-    BanIcon,
+    ClockIcon,
     BellIcon,
-    FlagIcon,
-    InboxIcon,
     MenuIcon,
-    PencilAltIcon,
-    UserCircleIcon,
     XIcon
 } from '@heroicons/vue/outline'
 import AlternativeProfilePicture from '../../../../assets/images/profile_picture.png'
@@ -193,12 +193,7 @@ import moment from 'moment'
 import Notifications from '../../global/Notifications'
 
 const sidebarNavigation = [
-    { name: 'Open', href: '#', icon: InboxIcon, current: true },
-    { name: 'Archive', href: '#', icon: ArchiveIcon, current: false },
-    { name: 'Customers', href: '#', icon: UserCircleIcon, current: false },
-    { name: 'Flagged', href: '#', icon: FlagIcon, current: false },
-    { name: 'Spam', href: '#', icon: BanIcon, current: false },
-    { name: 'Drafts', href: '#', icon: PencilAltIcon, current: false }
+    { name: 'Time-Tracker', href: 'timetracker.render.index', icon: ClockIcon }
 ]
 const userNavigation = [
     { name: 'Your Profile', href: '#' },
