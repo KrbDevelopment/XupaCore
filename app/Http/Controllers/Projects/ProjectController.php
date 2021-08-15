f8b66c67bdf0a0f30f7ca266c38ff98abbd62475
Nino Gralla <nino.gralla@gmx.de>
2021-08-15 15:20:43 +0200

    BACK - add Fileupload functions to ProjectController

--- app/Http/Controllers/Projects/ProjectController.php
+++ app/Http/Controllers/Projects/ProjectController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Projects;
 
 use App\Http\Controllers\Controller;
 use App\Models\Project;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class ProjectController extends Controller
 {
@@ -21,14 +23,20 @@ class ProjectController extends Controller
         ]);
     }
 
-    public function createProject(Request $request) {
+    /**
+     * Create Project
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function createProject(Request $request): RedirectResponse
+    {
         $request->validate([
            'title' => 'required|string|max:255',
            'slogan' => 'required|string|max:255',
            'description' => 'required|string',
 
-           'thumbnail' => 'nullable|string',
-           'logo' => 'nullable|string',
+           'thumbnail' => 'nullable|image|max:5120',
+           'logo' => 'nullable|image|max:5120',
 
            'website_title' => 'nullable|string|max:255',
            'website_link' => 'nullable|string|max:255',
@@ -44,8 +52,8 @@ class ProjectController extends Controller
         $project->title = $request->title;
         $project->slogan = $request->slogan;
         $project->description = $request->description;
-        $project->thumbnail = $request->thumbnail;
-        $project->logo = $request->logo;
+        $project->thumbnail = $this->storeThumbnail($request->thumbnail);
+        $project->logo = $this->storeLogo($request->logo);
         $project->website_title = $request->website_title;
         $project->website_link = $request->website_link;
         $project->location_address = $request->location_address;
@@ -54,6 +62,92 @@ class ProjectController extends Controller
         $project->save();
 
         return back();
+    }
+
+    /**
+     * Project Thumbnails
+     */
+
+    /**
+     * Set Project thumbnail helper
+     * @param $image Image Request Image (must be validated)
+     * @return string|null String with the path of the image
+     */
+    public function storeThumbnail($image): ?string
+    {
+        if (!$image) return null;
+        return $image->store('project_thumbnails');
+    }
+
+    /**
+     * Update Project thumbnail
+     * @param Request $request
+     * @param Project $project
+     * @return RedirectResponse
+     */
+    public function updateThumbnail(Request $request, Project $project): RedirectResponse
+    {
+        $request->validate([
+            'thumbnail'=>'required|image|max:5120'
+        ]);
+
+        $project->setThumbnail($request->file("thumbnail"));
+        $project->save();
+
+        return back();
+    }
+
+    /**
+     * Get Project Thumbnail helper
+     * @param Project $project
+     * @return StreamedResponse
+     */
+    public function getThumbnail(Project $project): StreamedResponse
+    {
+        return $project->getThumbnail();
+    }
+
+    /**
+     * Project Logos
+     */
+
+    /**
+     * Set Project logo helper
+     * @param $image Image Request Image (must be validated)
+     * @return string|null String with the path of the image
+     */
+    public function storeLogo($image): ?string
+    {
+        if (!$image) return null;
+        return $image->store('project_logos');
+    }
+
+    /**
+     * Update Project logos
+     * @param Request $request
+     * @param Project $project
+     * @return RedirectResponse
+     */
+    public function updateLogo(Request $request, Project $project): RedirectResponse
+    {
+        $request->validate([
+            'logo'=>'required|image|max:5120'
+        ]);
+
+        $project->setLogo($request->file("logo"));
+        $project->save();
+
+        return back();
+    }
+
+    /**
+     * Get Project Logo helper
+     * @param Project $project
+     * @return StreamedResponse
+     */
+    public function getLogo(Project $project): StreamedResponse
+    {
+        return $project->getLogo();
     }
 
 }
