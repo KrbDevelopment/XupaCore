6b7c480b0708ce6d09072a8f2ab42ac790823fb9
Nino Gralla <nino.gralla@gmx.de>
2021-08-15 15:20:21 +0200

    BACK - add Helper functions to Project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class Project extends Model
 {
@@ -36,4 +38,54 @@ class Project extends Model
         'website_link',
         'website_title'
     ];
+
+    /**
+     * Get Project thumbnail helper
+     * @return StreamedResponse|null
+     */
+    public function getThumbnail(): ?StreamedResponse
+    {
+        if (!$this->thumbnail) return null;
+        if (str_starts_with($this->thumbnail, 'http')) return $this->thumbnail;
+        return Storage::response($this->thumbnail);
+    }
+
+    /**
+     * Set Project thumbnail helper
+     * @param $image Image Request Image (must be validated)
+     * @return string String with the path of the image
+     */
+    public function setThumbnail($image): string
+    {
+        $path = $image->store('project_thumbnails');
+        $this->thumbnail = $path;
+        $this->save();
+
+        return $path;
+    }
+
+    /**
+     * Get Project logo helper
+     * @return StreamedResponse|null
+     */
+    public function getLogo(): ?StreamedResponse
+    {
+        if (!$this->logo) return null;
+        if (str_starts_with($this->logo, 'http')) return $this->logo;
+        return Storage::response($this->logo);
+    }
+
+    /**
+     * Set Project logo helper
+     * @param $image Image Request Image (must be validated)
+     * @return string String with the path of the image
+     */
+    public function setLogo($image): string
+    {
+        $path = $image->store('project_logos');
+        $this->logo = $path;
+        $this->save();
+
+        return $path;
+    }
 }
