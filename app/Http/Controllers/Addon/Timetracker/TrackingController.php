73bf695baa57fc7ddf47f193678163a5d9ee8ce1
Nino Gralla <nino.gralla@gmx.de>
2021-09-20 21:19:10 +0200

    Add Tracking render functions

--- app/Http/Controllers/Addon/Timetracker/TrackingController.php
+++ app/Http/Controllers/Addon/Timetracker/TrackingController.php
@@ -11,7 +11,10 @@ class TrackingController extends Controller
     public function render_index() {
         return Inertia::render('Addon/Timetracker/Overview');
     }
-    public function render_single() {
-        return Inertia::render('Addon/Timetracker/Single');
+    public function render_session() {
+        return Inertia::render('Addon/Timetracker/Session');
     }
+    public function render_history() {
+            return Inertia::render('Addon/Timetracker/History');
+        }
 }
